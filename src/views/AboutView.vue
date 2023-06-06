170023a649cb279622700962238ce84836a6a255
qubaitian <2501055750@qq.com>
2023-06-06 18:22:46 +0800

    add .

---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <p> 投票网 </p>
+</template>
---
+++ src/views/VoteView.vue
@@ -0,0 +1,14 @@
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="count++">You clicked me!!!!!!! {{ count }} times.</button>
+</template>
